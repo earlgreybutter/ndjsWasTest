9af66d16296385dc112ee84b26131a17300ab1e7
earlgreybutter <duckduck31216@gmail.com>
2021-06-16 05:02:26 +0900

    initial commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const server = express();
+let words = require("./db/words.json");
+
+// console.log(words);
+// console.log(typeof words);
+// middleware 사용
+server.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layout.hbs",
+    partialsDir: "partials",
+  })
+);
+server.set("view engine", "hbs");
+server.use(express.static(__dirname + "/public"));
+server.use(bodyParser.urlencoded({ extended: false }));
+
+server.get("/", (req, res) => {
+  res.render("home", {
+    words,
+  });
+});
+
+server.post("/", (req, res) => {
+  const { query } = req.body;
+  res.render("home", {
+    words: words.filter((w) =>
+      w.word.toLowerCase().includes(query.toLowerCase())
+    ),
+  });
+});
+
+server.delete("/", (req, res) => {
+  let { word } = req.body;
+  words = words.filter((w) => !(w.word === word));
+});
+
+server.get("/add", (req, res) => {
+  res.render("add");
+});
+
+server.get("/quiz", (req, res) => {
+  res.render("quiz");
+});
+
+server.use((req, res) => {
+  res.render("404");
+});
+// GET
+// POST
+// DELETE
+// PUT
+
+server.listen(3000, (err) => {
+  console.log("The server is listening on port 3000.");
+});
